35a2261d494864190fa68901e0540b9debd66bc7
cesarhj19 <cesarhj19@gmail.com>
2019-09-24 18:29:15 -0700

    Got components to appear

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React from 'react';
 
-const NumberButton = () => {
+const NumberButton = props => {
   return (
-    <>
+    <button
+      className="number-button"
+      value={props.number}
+      onClick={props.onClick}
+    >
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+      {props.number}
+    </button>
   );
 };
+
+export default NumberButton;
--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from 'react';
 
 //import any components needed
+import SpecialButton from './SpecialButton';
 
 //Import your array data to from the provided data file
+import { specials } from '../../../data';
 
-const Specials = () => {
+const Specials = props => {
   // STEP 2 - add the imported data to state
-
+  const [specialState, setSpecialState] = useState(specials);
   return (
-    <div>
+    <div className="special-container">
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {specialState.map((elem, index) => (
+        <SpecialButton
+          className="specialButton"
+          special={elem}
+          key={index}
+          onClick={props.onClick}
+        />
+      ))}
     </div>
   );
 };
+
+export default Specials;
